7e6cf6950f806ac9a1b95a433b456805ee51d8b1
DarshiniBalamurugan <55129335+DarshiniBalamurugan@users.noreply.github.com>
2022-01-24 11:35:46 +0800

    export

--- process.php
+++ process.php
@@ -1,14 +1,16 @@
 <?php
-    $keys = array('id_number','first_name');
-    $csv_line = array();
-    foreach($keys as $key){
-        array_push($csv_line,'' . $_GET[$key]);
-    }
-    $fname = 'file_to_write_to.csv';
-    $csv_line = implode(',',$csv_line);
-    if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
-    $fcon = fopen($fname,'a');
-    $fcontent = $csv_line;
-    fwrite($fcon,$csv_line);
-    fclose($fcon);
-?>
+$fieldA = $_POST["sound"];
+$keys = array($fieldA); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
+$csv_line = $keys;
+ foreach( $keys as $key ){
+     array_push($csv_line,'' . $_GET[$key]);
+
+ }
+ $fname = 'data.csv'; //NAME OF THE FILE
+ $csv_line = implode(',',$csv_line);
+ if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
+ $fcon = fopen($fname,'a');
+ $fcontent = $csv_line;
+ fwrite($fcon,$csv_line);
+ fclose($fcon);
+ ?>
