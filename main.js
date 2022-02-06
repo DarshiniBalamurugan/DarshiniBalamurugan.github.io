9a37c4a8fbc4d71dc29b8e854b9eb9df5707490e
DarshiniBalamurugan <55129335+DarshiniBalamurugan@users.noreply.github.com>
2022-02-06 13:31:21 +0800

    form

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-// // Your web app's Firebase configuration
-//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyAd01hiizZK1FziPEadSuwcMHCS-nH5P7E",
-//     authDomain: "fyp-noise-pollution.firebaseapp.com",
-//     projectId: "fyp-noise-pollution",
-//     storageBucket: "fyp-noise-pollution.appspot.com",
-//     messagingSenderId: "704385174703",
-//     appId: "1:704385174703:web:d56f932a8cbb3d0683185a",
-//     measurementId: "G-J50HMNFJ0W"
-//   };
+// // Reference messages collection
+var messagesRef = firebase.database().ref('messages');
 
-//   // Initialize Firebase
-//   const app = initializeApp(firebaseConfig);
-//   const analytics = getAnalytics(app);
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var file_name=getInputVal('file_name')
+  var question1 = getInputVal('question1');
+  
+  // Save message
+  saveMessage(file_name, question1);
+
+//   // Show alert
+//   document.querySelector('.alert').style.display = 'block';
+
+//   // Hide alert after 3 seconds
+//   setTimeout(function(){
+//     document.querySelector('.alert').style.display = 'none';
+//   },3000);
+
+//   // Clear form
+//   document.getElementById('contactForm').reset();
+// }
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
