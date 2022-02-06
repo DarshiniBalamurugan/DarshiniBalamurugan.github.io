4f065dd93e9aa5bda3f26c006587afb553dd2abf
DarshiniBalamurugan <55129335+DarshiniBalamurugan@users.noreply.github.com>
2022-02-06 13:26:53 +0800

    firebase trial

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyAd01hiizZK1FziPEadSuwcMHCS-nH5P7E",
+//     authDomain: "fyp-noise-pollution.firebaseapp.com",
+//     projectId: "fyp-noise-pollution",
+//     storageBucket: "fyp-noise-pollution.appspot.com",
+//     messagingSenderId: "704385174703",
+//     appId: "1:704385174703:web:d56f932a8cbb3d0683185a",
+//     measurementId: "G-J50HMNFJ0W"
+//   };
 
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
